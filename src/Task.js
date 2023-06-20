20a0b2fd50d8825ef205003aa9272738f2482053
novita <ekasarinovita4@gmail.com>
2023-06-20 11:32:29 +0700

    uploadFirebase

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Novita Ekasari"; // TODO: replace this
+const studentId = "2006700025"; // TODO: replace this
+
+export { url, studentName, studentId };
